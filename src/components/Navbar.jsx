1bbb5b28297c6d7ca14817d964984c0d7f069112
Deni <deni.shaipp@gmail.com>
2023-05-25 22:21:55 +0300

    fixed navbar name formatting

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
-          <p className="text-white text-[18px] font-bold cursor-pointer">@MaliciousQ <span className="sm:block hidden">| Deni Shaipov </span></p>
+          <p className="text-white text-[18px] font-bold cursor-pointer flex">@MaliciousQ &nbsp; <span className="sm:block hidden">| Deni Shaipov </span></p>
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10">
           {
