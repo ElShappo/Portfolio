a6ff6fea587ff86a0dda1e00afcdb4eff0a7ebdb
Deni <deni.shaipp@gmail.com>
2023-05-25 22:14:48 +0300

    mobile menu added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
             setActive("");
             window.scrollTo(0, 0);
           }}
-          >
+        >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer">@MaliciousQ <span className="sm:block hidden">| Deni Shaipov </span></p>
         </Link>
@@ -43,6 +43,24 @@ const Navbar = () => {
             className="w-[28px] h-[28px] object-contain cursor-pointer"
             onClick={() => setToggle(!toggle)}
           />
+
+          <div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-20 rounded-xl`}>
+            <ul className="list-none flex flex-col justify-end items-start gap-4">
+            {
+              navLinks.map( (link) => (
+                <li key={link.id} className={`${
+                  active === link.title ? "text-white" : "text-secondary"
+                } hover:text-white font-poppins text-[16px] font-medium cursor-pointer`}
+                onClick={() => {
+                  setToggle(!toggle);
+                  setActive(link.title);
+                  }}>
+                  <a href={`#${link.id}`}>{link.title}</a>
+                </li>
+              ))
+            }
+          </ul>
+          </div>
         </div>
       </div>
     </nav>
