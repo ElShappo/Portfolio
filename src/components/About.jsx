3c5bd028bbbee7aafe1341649ce3bbc7e95e6713
Deni <deni.shaipp@gmail.com>
2023-06-16 10:35:39 +0300

    cards in 'About' section are ready

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,6 +6,23 @@ import { styles } from '../styles'
 import { services } from '../constants'
 import { fadeIn, textVariant } from '../utils/motion'
 
+const ServiceCard = ({index, title, icon}) => {
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div variants={fadeIn("right", "spring", 0.5 * index, 0.75)} className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'>
+        <div options={{
+          max: 45,
+          scale: 1,
+          speed: 450
+        }} className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
+          <img src={icon} alt={title} className="w-14 h-14 object-contain" />
+            <h3 className="text-[20px] text-white text-center font-bold">{title}</h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  )
+}
+
 const About = () => {
   return (
     <>
@@ -15,8 +32,14 @@ const About = () => {
       </motion.div>
 
       <motion.p variants={fadeIn("", "", 0.1, 1)} className="mt-4 text-secondary text-[17px] width-w-3xl leading-[30px]">
-      I'm really into writing web-applications. I love to use JavaScript with Vue.JS, Quasar or React in my projects. I also have experience in writing vanilla HTML/CSS (the latter, for instance, includes working with flexbox and grid). I am familiar with RestAPI and have experience in creating databases on PostgreSQL and utilizing Sequelize ORM, had a chance to try out and test Firebase Cloud Storage for my Quasagram project. I am a fast learner and really looking forward to sky-rocket my learning path with hard work and persistence.
+        I'm really into writing web-applications. I love to use JavaScript with Vue.JS, Quasar or React in my projects. I also have experience in writing vanilla HTML/CSS (the latter, for instance, includes working with flexbox and grid). I am familiar with RestAPI and have experience in creating databases on PostgreSQL and utilizing Sequelize ORM, had a chance to try out and test Firebase Cloud Storage for my Quasagram project. I am a fast learner and really looking forward to sky-rocket my learning path with hard work and persistence.
       </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
     </>
   )
 }
