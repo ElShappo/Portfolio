ef72e999308793c26765909db692a3e99fc2cf54
Deni Shaipov <deni.shaipp@gmail.com>
2023-06-17 23:27:26 +0300

    anchor of scroll animated label is now working

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,12 +22,12 @@ const Hero = () => {
             <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
               <motion.dev
                 animate={{
-                  y: [0, 24, 0]
+                  y: [0, 24]
                 }}
                 transition={{
                   duration: 1.5,
                   repeat: Infinity,
-                  repeatType: 'loop'
+                  repeatType: 'reverse'
                 }}
                 className="w-3 h-3 rounded-full bg-secondary mb-1"
               />
