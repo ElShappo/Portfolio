0ec73638a7aeeee7a4193418c2286cfe9f7d346e
Deni Shaipov <deni.shaipp@gmail.com>
2023-06-18 22:57:24 +0300

    contact form is ready

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -13,10 +13,67 @@ const Contact = () => {
     name: '',
     email: '',
     message: '',
-  })
+  });
+  const [loading, setLoading] = useState(false);
+  
+  const handleChange = (e) => {}
+
+  const handleSubmit = (e) => {}
+
   return (
-    <div>Contact</div>
+    <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
+      <motion.div
+        variants={slideIn('left', 'tween', 0.2, 1)}
+        className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
+      >
+      <p className={styles.sectionSubText}>Get in touch</p>
+      <h3 className={styles.sectionHeadText}>Contact.</h3>
+
+      <form ref={formRef} onSubmit={handleSubmit} className='mt-12 flex flex-col gap-8'>
+        <label className="flex flex-col">
+          <span className="font-medium mb-4">Your name</span>
+          <input
+            type="text"
+            name="name"
+            value={form.name}
+            onChange={handleChange}
+            placeholder="What's your name?"
+            className="bg-tertiary py-4 px-6 placeholder: text-secondary rounded-lg text-white outlined-none border-none font-medium"
+          />
+        </label>
+        <label className="flex flex-col">
+          <span className="font-medium mb-4">Your email</span>
+          <input
+            type="email"
+            name="email"
+            value={form.email}
+            onChange={handleChange}
+            placeholder="What's your email?"
+            className="bg-tertiary py-4 px-6 placeholder: text-secondary rounded-lg text-white outlined-none border-none font-medium"
+          />
+        </label>
+        <label className="flex flex-col">
+          <span className="font-medium mb-4">Your message</span>
+          <textarea
+            rows="7"
+            name="message"
+            value={form.message}
+            onChange={handleChange}
+            placeholder="What do you want to say?"
+            className="bg-tertiary py-4 px-6 placeholder: text-secondary rounded-lg text-white outlined-none border-none font-medium"
+          />
+        </label>
+        <button
+          type="submit"
+          className="text-white bg-blue-950 py-4 px-8 outline-none text-white w-fit font-bold shadow-md rounded-xl"
+        >
+          {loading ? "Sending..." : 'Send'}
+        </button>
+      </form>
+
+      </motion.div>
+    </div>
   )
 }
 
-export default SectionWrapper(Contact, "")
+export default SectionWrapper(Contact, "contact");
